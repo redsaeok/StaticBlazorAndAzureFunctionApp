6f9722495498dd2cef7c8379bd5a2061f59c015a
redsaeok <74396365+redsaeok@users.noreply.github.com>
2024-03-06 20:23:52 -0800

    1st

---
+++ SchoolSPA/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolSPA;
+
+public class Constants
+{
+    //public const string SchoolAPIBase = "http://localhost:7071/api/";
+    //public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
+    public const string SchoolAPIBase = "/api/";
+}
